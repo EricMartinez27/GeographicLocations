c1093cdb316db7c64340b937240d7486106e37d2
LTI <LTI@INFEDL31055.lntinfotech.com>
2021-09-23 16:52:25 -0500

    Got location names with positions from json file

--- consoleProject/Program.cs
+++ consoleProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using libraryProject;
 
 namespace consoleProject
 {
@@ -6,7 +7,13 @@ namespace consoleProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var locations = Fetching.GetDataFromHttp().GetAwaiter().GetResult();
+
+            foreach(var location in locations)
+            {
+                Console.WriteLine($"{location.name}: [{location.position[0]}, {location.position[1]}]");
+            }
+
         }
     }
 }
---
+++ libraryProject/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace libraryProject
+{
+    public record Location(string name,  float[] position);
+
+    public class Fetching
+    {
+        public static async Task<List<Location>> GetDataFromHttp()
+        {
+            var client = new HttpClient();
+            var Locations = await client.GetFromJsonAsync<List<Location>>("https://raw.githubusercontent.com/chyld/datasets/main/markers.json");
+           
+
+            // if (jsonTask.Status != TaskStatus.RanToCompletion) throw new Exception("JSON Error");
+
+            return Locations;      
+
+        }
+    }
+}
