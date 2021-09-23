fe5ff79faff1195ea8755bdaa631fd95ac1ee2ff
LTI <LTI@INFEDL31055.lntinfotech.com>
2021-09-23 15:43:56 -0500

    first commit

---
+++ consoleProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
